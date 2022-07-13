8374067c2aae0412f3a1857d1c59eb31ca9574aa
Claudio Guerra <claudevandort@gmail.com>
2022-07-13 15:56:40 +0800

    feat(player): move player to its own class

--- sky_dodge.py
+++ sky_dodge.py
@@ -9,11 +9,21 @@ from pygame.locals import (
     QUIT
 )
 
-pygame.init()
-
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 75))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+
+pygame.init()
+
 screen = pygame.display.set_mode(SCREEN_SIZE)
+player = Player()
 
 '''
 Game Loop
@@ -30,15 +40,8 @@ while running:
                 running = False
         elif event.type == QUIT:
             running = False
-    screen.fill((255, 255, 255))
-    surf = pygame.Surface((50, 50))
-    surf.fill((0, 0, 0))
-    rect = surf.get_rect()
-    surf_center = (
-        (SCREEN_WIDTH-surf.get_width())/2,
-        (SCREEN_HEIGHT-surf.get_height())/2
-    )
-    screen.blit(surf, surf_center)
+    screen.fill((0, 0, 0))
+    screen.blit(player.surf, player.rect)
     pygame.display.flip()
 
 pygame.quit()
